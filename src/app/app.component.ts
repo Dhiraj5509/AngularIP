6c6febef788c5b6804246561b55025413aca1f74
Dhiraj Dunani <dhiraj@infocusp.in>
2021-12-22 16:09:51 +0530

    created new components and used the components in other components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
+// Component 
+// 1) selector 
+// 2) url 
+// 3) html file url(template url)
+
 import { Component } from '@angular/core';
 
+// we usually add a decorator "@Component" to give that class the power of the component
+// @Component({})
 @Component({
+
+  // the matching tag needed in index.html / template file
+  // the selector must be unique and shouldn't collide with the other selectors
   selector: 'app-root',
+  // the html file needed to be served when this component is served
   templateUrl: './app.component.html',
+  // the styles to apply to the html file rendered
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
+
+  // we use declarations to let know which components are we using
   declarations: [
-    AppComponent
+    AppComponent , 
+    ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent 
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
+
+  // which component/s should bootstrap be aware of at the time of start-up
   bootstrap: [AppComponent]
 })
 export class AppModule { }
