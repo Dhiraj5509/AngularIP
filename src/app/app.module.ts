b9e90758cda3ad326d7d20585ca358c667a3b805
Dhiraj Dunani <dhiraj@infocusp.in>
2021-12-23 12:37:56 +0530

    binding using ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 @NgModule({
@@ -12,7 +12,8 @@ import { ServersComponent } from './servers/servers.component';
    
   ],
   imports: [
-    BrowserModule
+    BrowserModule , 
+    FormsModule
   ],
   providers: [],
 
