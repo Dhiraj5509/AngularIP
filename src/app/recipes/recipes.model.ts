da0e87953506572ab3cbff5bce36ee1d4ff9622d
Dhiraj Dunani <dhiraj@infocusp.in>
2021-12-24 14:57:37 +0530

    Shopping list skeleton added

---
+++ src/app/recipes/recipes.model.ts
@@ -0,0 +1,14 @@
+// recipe model
+export class Recipe{
+    public name : string ; 
+    public imagePath : string ; 
+    public description : string ; 
+
+    constructor(name:string , imagePath:string , description:string)
+    {
+        this.name = name ; 
+        this.imagePath = imagePath ; 
+        this.description= description; 
+    }
+    
+}
