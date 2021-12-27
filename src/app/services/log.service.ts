d23a5697346a3cabc7e15563bbb2afcb750e698c
Dhiraj Dunani <dhiraj@infocusp.in>
2021-12-27 14:40:50 +0530

    dependency injection concepts well understood

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+// this class is a normal service , it doesn't require any dependency 
+// so we are not placing the special metadata i.e. @Decorator
 export class LogService {
 
   constructor() { }
