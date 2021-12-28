4a6796d5d9c6c7be2991c20cbcd62e87931190a7
Dhiraj Dunani <dhiraj@infocusp.in>
2021-12-28 13:58:12 +0530

    accessing path param part2

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private activatedRoute : ActivatedRoute) { }
 
   ngOnInit() {
+    // using ActivatedRoute , we can access the data passes using path param
+    this.user = {
+      id : this.activatedRoute.snapshot.params['id'] , 
+      name : this.activatedRoute.snapshot.params['name'] 
+    }
+    // if we request the same component again , angular doesn't re-instantiate it 
+    // in our case the query parameters change , but the component isn't changed
+    // therefore in order to have an updated params , we can have a callback  , and if the params changes the callback will be called 
+    this.activatedRoute.params.subscribe( (params : Params)=>{
+      this.user.id = params['id']
+      this.user.name = params['name']
+    } );
+      
   }
 
 }
