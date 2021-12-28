9855adef3c586dbf85c9dbb83b37f88f778e445e
Dhiraj Dunani <dhiraj@infocusp.in>
2021-12-28 12:01:39 +0530

    routing programatically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router , RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,14 +11,18 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 // specifying our routes
 const appRoutes : Routes = [
   // it will take key value pairs {path , component } 
-  {path:'users' , component:UsersComponent}  , // localhost:4200/users -> call users component
-  {path:'home' , component:HomeComponent} , 
+  // the path we have used here is absolute path 
+  // pathMatch is used to say angular whether we want full match or prefix match
+  {path:'users' , component:UsersComponent , pathMatch:"full"} , // localhost:4200/users -> call users component
+  {path:'home' , component:HomeComponent , pathMatch:"full"} , 
+  {path:'servers' , component:ServersComponent , pathMatch:"full"} , 
   {path:'' , component:HomeComponent} , 
-  {path:'servers' , component:ServersComponent} , 
+  { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
 ];
 @NgModule({
   declarations: [
@@ -28,7 +32,8 @@ const appRoutes : Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
@@ -9,10 +9,10 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService , private router : Router) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
-
+  
 }
