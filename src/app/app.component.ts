e7f298bcf28164b876a0424ea4806842ae3790df
Dhiraj Dunani <dhiraj@infocusp.in>
2021-12-29 16:10:12 +0530

    form handling , passing form details to the componnent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent {
+  suggestName = ""
+  suggestUserName() {
+    this.suggestName = 'Superuser';
+  }
 
-  ngOnInit() {}
+  onSubmit(formElements : NgForm)
+  {
+    console.log(formElements); 
+    
+  }
+
 }
