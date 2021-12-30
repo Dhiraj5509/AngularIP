cf2ded4582280564388c6703dbb76a15b07cfc53
Dhiraj Dunani <dhiraj@infocusp.in>
2021-12-30 11:38:31 +0530

    called api to get data using http module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  defaultValue : string = "pet"; 
-  suggestName = ""
-  suggestUserName() {
-    this.suggestName = 'Superuser';
-  }
+export class AppComponent implements OnInit{
+  lis :{name:string , position:string , office:string , salary:number}[]= [] ; 
+  li : any ; 
+  constructor(private http : HttpClient){
 
-  onSubmit(formElements : NgForm)
-  {
-    console.log(formElements); 
-    
+  }
+  ngOnInit(): void {
+    let observable : Observable<Object> = this.http.get('http://www.mocky.io/v2/5ea172973100002d001eeada')! ; 
+
+    observable.subscribe((data)=>{
+      if(data){
+
+      }
+      this.li=data;
+      this.lis=this.li.list;
+    }) ; 
+   
   }
 
 }
